51572d9bc4f6df3ddd65ac31f7367b0dd985a40a
Likki <antonenko.lidia@gmail.com>
2018-08-07 23:05:33 +0300

    Corrected some mistakes in decode_move. Added decode_line.

--- decode.py
+++ decode.py
@@ -5,11 +5,19 @@ from operations import possible_moves_from_position
 import copy
 
 
+class DecodeError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
 def decode_move(short_line, board, player_color):
-    if short_line == 'O-O':
+    if short_line == '0-0':
         return 'e1g1' if player_color == WHITE else 'e8g8'
 
-    if short_line == 'O-O-O':
+    if short_line == '0-0-0':
         return 'e1c1' if player_color == WHITE else 'e8c8'
 
     # FIXME: add checks that +, #, x are correct: really suspense, or gameover, or figure eaten
@@ -53,7 +61,7 @@ def decode_move(short_line, board, player_color):
 
     candidates = figures_on_board(board, type=figure_type, color=player_color)
     if initial_x:
-        candidates = list(filter(lambda item: item[1].x == INDEX_TO_LETTER(initial_x), candidates))
+        candidates = list(filter(lambda item: item[1].x == LETTER_TO_INDEX[initial_x], candidates))
     elif initial_y:
         candidates = list(filter(lambda item: item[1].y == initial_y - 1, candidates))
 
@@ -68,3 +76,58 @@ def decode_move(short_line, board, player_color):
     if len(candidates) > 1:
         raise RuntimeError("Too many suitable figures for turn")
     return str(candidates[0][1]) + end_field
+
+
+
+def decode_game(line):
+    def end_word(line):
+        result = line.find(' ')
+        if result == -1:
+            return len(line)
+        return result
+
+    game_results = ['1-0', '0-1', '1/2']
+    human_readable = []
+    n = 1
+    board = Board()
+    while line:
+        print("Turn #{0}".format(n))
+        for game_result in game_results:
+            if line.startswith(game_result):
+                human_readable += game_result
+                return human_readable
+
+        prefix = str(n) + '.'
+        if not line.startswith(prefix):
+            raise DecodeError("Expected turn number: {0}".format(line))
+        line = line[len(prefix):]
+
+        line = line.lstrip(' ')
+        next_space = end_word(line)
+        white_turn = line[:next_space]
+        try:
+            decoded = decode_move(white_turn, board, WHITE)
+        except:
+            raise DecodeError("Expected correct move description: `{0}`".format(white_turn))
+        human_readable += [decoded]
+        board.move(Coordinates.from_string(decoded[:2]), Coordinates.from_string(decoded[2:]))
+
+        line = line[next_space:].lstrip(' ')
+        for game_result in game_results:
+            if line.startswith(game_result):
+                human_readable += game_result
+                return human_readable
+
+        next_space = end_word(line)
+        black_turn = line[:next_space]
+        try:
+            decoded = decode_move(black_turn, board, BLACK)
+        except:
+            raise DecodeError("Expected correct move description: `{0}`".format(black_turn))
+        human_readable += [decoded]
+        board.move(Coordinates.from_string(decoded[:2]), Coordinates.from_string(decoded[2:]))
+        line = line[next_space:].lstrip(' ')
+
+        n += 1
+
+    raise DecodeError("Expected game result")
