2896a55189f03ea59ea7bc58173b53478abd3b23
arhip <arhip.arhip2002@yandex.ru>
2018-10-01 22:14:03 +0300

    Game status

---
+++ game_status.py
@@ -0,0 +1,19 @@
+from board import board
+
+
+
+
+class GameCondition(object):
+	def __init__(self,game_type,player1,player2):
+		self.game_type
+		self.player1
+		self.player2
+		self.board_list = []
+	def add_board(self.board):
+		for i in self.board_list:
+			if board = i[0]:
+				i[1]+=1
+				return True
+		self.board_list.append((board,1))
+
+
